72cd50bfc1fca9315deceb66c51ab4d164e6b93f
Maxime Tyler <127086@supinfo.com>
2015-02-05 11:57:54 +0100

    fix SSR when `transition.redirect` is called

--- server/server.router.jsx
+++ server/server.router.jsx
@@ -6,7 +6,22 @@ const Router = require('react-router');
 const routes = require('../app/routes');
 
 module.exports = (req, res) => {
-  Router.run(routes, req.url, (Handler) => {
+  let router = Router.create({
+    routes: routes,
+    location: req.url,
+    onAbort: function (redirect) {
+      // TODO: Try to render the good page with re-creating a Router,
+      // and with modifying req with `redirect.to`
+      res.writeHead(303, {'Location': redirect.to});
+      return res.send();
+    },
+    onError: function (err) {
+      console.log('Routing Error');
+      console.log(err);
+    }
+  });
+
+  router.run((Handler) => {
     let content = React.renderToString(<Handler/>);
     return res.render('main', {content});
   });
