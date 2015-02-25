c1cf817b27558ccd4e0d19e7df0ffbf537203c9a
Maxime Tyler <127086@supinfo.com>
2015-02-25 16:52:21 +0100

    feat(resolver): use `react-resolver` on client side too

--- app/main.jsx
+++ app/main.jsx
@@ -2,11 +2,15 @@
 
 import React from 'react';
 import Router from 'react-router';
+import Resolver from 'react-resolver';
 
 import routes from './routes';
 
+const resolver = new Resolver();
 const content = document.getElementById('content');
 
-Router.run(routes, Router.HistoryLocation, (Handler) => {
-  return React.render(<Handler/>, content);
-});
+Router.run(
+  resolver.route(routes),
+  Router.HistoryLocation,
+  (Handler) => React.render(<Handler/>, content)
+);
