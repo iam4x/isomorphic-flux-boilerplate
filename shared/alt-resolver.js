334e81161aa6d80a8b2bc83e8159fc28793b7535
iam4x <iam4x@me.com>
2015-04-02 15:55:34 +0200

    feat(alt.resolver): drop `async` library, use `Promise.all`

--- shared/alt-resolver.js
+++ shared/alt-resolver.js
@@ -1,29 +1,23 @@
 'use strict';
 
 import _ from 'lodash';
-import async from 'async';
 
 import services from './services';
 
 export default (params, callback) => {
-  let toResolve = [];
+  const toResolve = [];
   // Find which services we need to resolve
   // before rendering
   params.routes.forEach((route) => {
     if (typeof services[route.name] === 'function') {
-      toResolve.push(services[route.name]);
+      toResolve.push(new Promise(services[route.name]));
     }
   });
-  // Resolve them, we use `async.map` since they
-  // are async functions
-  async.map(
-    toResolve,
-    (service, done) => service(done),
-    (err, results) => {
+  Promise
+    .all(toResolve)
+    .then((results) => {
       let nextState = {};
       results.forEach((result) => nextState = _.assign(nextState, result));
-      // All services are resolved, let's fire the callback
       return callback(JSON.stringify(nextState));
-    }
-  );
+    });
 };
--- shared/services.js
+++ shared/services.js
@@ -2,14 +2,22 @@
 
 import request from 'superagent';
 
-export default {
-  users(done) {
+import UserStore from 'stores/user';
+
+const fetchUsers = (resolve, reject) => {
+  const users = UserStore.getState().users;
+  // Check if store has already data
+  // before request new data.
+  if (users && users.length > 0) {
+    return resolve({UserStore: {users}});
+  }
+  else {
     return request
       .get('http://api.randomuser.me/?results=10')
-      .end((response) => {
-        return done(null, {
-          UserStore: {users: response.body.results}
-        });
-      });
+      .end((response) => resolve({UserStore: {users: response.body.results}}));
   }
+};
+
+export default {
+  users: fetchUsers
 };
