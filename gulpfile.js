ea9a72d5057560b402fd5bf292565fa4e85dcbd2
iam4x <iam4x@me.com>
2015-04-07 12:18:19 +0200

    feat(karma): remove `gulp-karma` use native `karma.server`

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 'use strict';
 
+/* eslint camelcase: 0 */
+
+var path = require('path');
 var gulp = require('gulp');
 var del = require('del');
 var gutil = require('gulp-util');
@@ -11,11 +14,11 @@ var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var minifyCSS = require('gulp-minify-css');
 var concatCss = require('gulp-concat-css');
-var karma = require('gulp-karma');
 var eslint = require('gulp-eslint');
 var pngquant = require('imagemin-pngquant');
 var proxy = require('proxy-middleware');
 var webpack = require('webpack');
+var karma = require('karma').server;
 var WebpackDevServer = require('webpack-dev-server');
 var objectAssign = require('object-assign');
 
@@ -36,15 +39,15 @@ var CODE_FILES = [
 // +--------------+
 
 gulp.task('clean:img', function () {
-  return del([__dirname + '/dist/img/**/*']);
+  return del(['dist/img/**/*']);
 });
 
 gulp.task('clean:css', function () {
-  return del([__dirname + '/dist/css/**/*']);
+  return del(['dist/css/**/*']);
 });
 
 gulp.task('clean:js', function () {
-  return del([__dirname + '/dist/js/**/*']);
+  return del(['dist/js/**/*']);
 });
 
 gulp.task('lint:code', function () {
@@ -85,59 +88,63 @@ gulp.task('connect', function () {
         webpackProxy(),
         expressProxy()
       ]
-    },
+    }
   });
 });
 
-gulp.task('webpack-dev-server', ['clean:js'], function () {
-  var config = objectAssign({}, webpackConfig);
-  config.devtool = 'source-map';
-  config.debug = true;
-  config.entry = [
-    'webpack-dev-server/client?http://localhost:8090',
-    'webpack/hot/only-dev-server',
-    __dirname + '/app/index.js'
-  ];
-  config.module.loaders = [{
-    test: /\.js/,
-    loaders: ['react-hot', 'babel-loader'],
-    exclude: /node_modules/
-  }];
-  config.plugins = [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
-  ];
+gulp.task(
+  'webpack-dev-server',
+  ['lint:code', 'clean:js'],
+  function () {
+    var config = objectAssign({}, webpackConfig);
+    config.devtool = 'source-map';
+    config.debug = true;
+    config.entry = [
+      'webpack-dev-server/client?http://localhost:8090',
+      'webpack/hot/only-dev-server',
+      path.join(__dirname, '/app/index.js')
+    ];
+    config.module.loaders = [{
+      test: /\.js/,
+      loaders: ['react-hot', 'babel-loader'],
+      exclude: /node_modules/
+    }];
+    config.plugins = [
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NoErrorsPlugin()
+    ];
 
-  new WebpackDevServer(webpack(config), {
-    publicPath: config.output.publicPath,
-    hot: true,
-    color: true,
-    stats: {
-      assets: true,
-      colors: true,
-      version: false,
-      hash: false,
-      timings: true,
-      chunks: false,
-      chunkModules: false
-    }
-  }).listen(8090, 'localhost', function (err) {
-    if (err) {
-      throw new gutil.PluginError('webpack-dev-server', err);
-    }
-    gutil.log('[webpack-dev-server]', 'started on port 8090');
-  });
-});
+    new WebpackDevServer(webpack(config), {
+      publicPath: config.output.publicPath,
+      hot: true,
+      color: true,
+      stats: {
+        assets: true,
+        colors: true,
+        version: false,
+        hash: false,
+        timings: true,
+        chunks: false,
+        chunkModules: false
+      }
+    }).listen(8090, 'localhost', function (err) {
+      if (err) {
+        throw new gutil.PluginError('webpack-dev-server', err);
+      }
+      gutil.log('[webpack-dev-server]', 'started on port 8090');
+    });
+  }
+);
 
 gulp.task('supervisor', function () {
-  supervisor(__dirname + '/server/index.js', {
+  supervisor('server/index.js', {
     watch: ['server'],
     extensions: ['jsx', 'js']
   });
 });
 
 gulp.task('images', ['clean:img'], function () {
-  return gulp.src(__dirname + '/app/images/**/*')
+  return gulp.src('app/images/**/*')
     .pipe(imagemin({
       optimizationLevel: 3,
       progressive: true,
@@ -145,36 +152,30 @@ gulp.task('images', ['clean:img'], function () {
       svgoPlugins: [{removeViewBox: false}],
       use: [pngquant()]
     }))
-    .pipe(gulp.dest(__dirname + '/dist/img'))
+    .pipe(gulp.dest('dist/img'))
     .pipe(size());
 });
 
 gulp.task('sass', function () {
-  return gulp.src(__dirname + '/app/styles/**/*.scss')
+  return gulp.src('app/styles/**/*.scss')
     .pipe(sourcemaps.init())
     .pipe(sass({errLogToConsole: true}))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest(__dirname + '/.tmp/css'));
+    .pipe(gulp.dest('.tmp/css'));
 });
 
 gulp.task('styles', ['clean:css', 'sass'], function () {
-  return gulp.src(__dirname + '/.tmp/css/**/*.css')
+  return gulp.src('.tmp/css/**/*.css')
     .pipe(concatCss('styles.css'))
-    .pipe(gulp.dest(__dirname + '/dist/css'))
+    .pipe(gulp.dest('dist/css'))
     .pipe(browserSync.reload({stream: true}));
 });
 
-gulp.task('test:watch', function () {
-  return gulp.src(__dirname + 'test/spec/**/*.test.jsx')
-    .pipe(karma({
-      configFile: 'karma.conf.js',
-      action: 'watch'
-    }))
-    .on('error', console.warn);
+gulp.task('test:watch', function (done) {
+  karma.start({configFile: path.join(__dirname, 'karma.conf.js')}, done);
 });
 
 gulp.task('dev', [
-  'lint:code',
   'images',
   'styles',
   'supervisor',
@@ -183,81 +184,79 @@ gulp.task('dev', [
   'test:watch'
 ], function () {
   gulp.watch(CODE_FILES, ['lint:code']);
-  gulp.watch(__dirname + '/app/images/**/*', ['images']);
-  gulp.watch(__dirname + '/app/styles/**/*', ['styles']);
+  gulp.watch('app/images/**/*', ['images']);
+  gulp.watch('app/styles/**/*', ['styles']);
 });
 
 // +-------------+
 // | BUILD TASKS |
 // +-------------+
 
-gulp.task('webpack:build', ['clean:js'], function (callback) {
-  var config = objectAssign({}, webpackConfig);
-  config.plugins = [
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production')
-      }
-    }),
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false,
-        screw_ie8: true,
-        sequences: true,
-        dead_code: true,
-        drop_debugger: true,
-        comparisons: true,
-        conditionals: true,
-        evaluate: true,
-        booleans: true,
-        loops: true,
-        unused: true,
-        hoist_funs: true,
-        if_return: true,
-        join_vars: true,
-        cascade: true,
-        drop_console: true
-      },
-      output: {
-        comments: false
-      },
-      sourceMap: false
-    })
-  ];
+gulp.task(
+  'webpack:build',
+  ['lint:code', 'test:build', 'clean:js'],
+  function (callback) {
+    var config = objectAssign({}, webpackConfig);
+    config.plugins = [
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: JSON.stringify('production')
+        }
+      }),
+      new webpack.optimize.DedupePlugin(),
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false,
+          screw_ie8: true,
+          sequences: true,
+          dead_code: true,
+          drop_debugger: true,
+          comparisons: true,
+          conditionals: true,
+          evaluate: true,
+          booleans: true,
+          loops: true,
+          unused: true,
+          hoist_funs: true,
+          if_return: true,
+          join_vars: true,
+          cascade: true,
+          drop_console: true
+        },
+        output: {
+          comments: false
+        },
+        sourceMap: false
+      })
+    ];
 
-  webpack(config, function (err, stats) {
-    if (err) {
-      throw new gutil.PluginError('webpack:build', err);
-    }
-    gutil.log('[webpack:build]', stats.toString({
-      colors: true
-    }));
-    return callback();
-  });
-});
+    webpack(config, function (err, stats) {
+      if (err) {
+        throw new gutil.PluginError('webpack:build', err);
+      }
+      gutil.log('[webpack:build]', stats.toString({
+        colors: true
+      }));
+      return callback();
+    });
+  }
+);
 
 gulp.task('build:styles', ['styles'], function () {
-  return gulp.src(__dirname + '/dist/css/styles.css')
+  return gulp.src('dist/css/styles.css')
     .pipe(minifyCSS())
-    .pipe(gulp.dest(__dirname + '/dist/css/'));
+    .pipe(gulp.dest('dist/css/'));
 });
 
-gulp.task('test:build', function () {
-  return gulp.src(__dirname + 'test/spec/**/*.test.jsx')
-    .pipe(karma({
-      configFile: 'karma.conf.js',
-      action: 'run'
-    }))
-    .on('error', function (err) {
-      throw err;
-    });
+gulp.task('test:build', function (done) {
+  karma.start({
+    configFile: path.join(__dirname, 'karma.conf.js'),
+    singleRun: true
+  }, done);
 });
 
 
 gulp.task('build', [
-  'lint:code',
-  'test:build',
   'build:styles',
   'images',
   'webpack:build'
