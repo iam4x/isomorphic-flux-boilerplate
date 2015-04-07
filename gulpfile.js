67a9411c15e0fbcb83d3de75073f35f780827575
iam4x <iam4x@me.com>
2015-04-07 15:29:46 +0200

    feat(gulp): use `run-sequence`

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,7 @@ var webpack = require('webpack');
 var karma = require('karma').server;
 var WebpackDevServer = require('webpack-dev-server');
 var objectAssign = require('react/lib/Object.assign');
+var runSequence = require('run-sequence');
 
 // load gulp plugins
 var $ = require('gulp-load-plugins')();
@@ -32,17 +33,7 @@ var CODE_FILES = [
 // | SHARED TASKS |
 // +--------------+
 
-gulp.task('clean:img', function () {
-  return del(['dist/img/**/*']);
-});
-
-gulp.task('clean:css', function () {
-  return del(['dist/css/**/*']);
-});
-
-gulp.task('clean:js', function () {
-  return del(['dist/js/**/*']);
-});
+gulp.task('clean', del.bind(null, ['.tmp', 'dist/*'], {dot: true}));
 
 gulp.task('lint:code', function () {
   return gulp.src(CODE_FILES)
@@ -91,49 +82,45 @@ gulp.task('connect', function () {
   });
 });
 
-gulp.task(
-  'webpack-dev-server',
-  ['lint:code', 'clean:js'],
-  function () {
-    var config = objectAssign({}, webpackConfig);
-    config.devtool = 'source-map';
-    config.debug = true;
-    config.entry = [
-      'webpack-dev-server/client?http://localhost:8090',
-      'webpack/hot/only-dev-server',
-      path.join(__dirname, '/app/index.js')
-    ];
-    config.module.loaders = [{
-      test: /\.js/,
-      loaders: ['react-hot', 'babel-loader'],
-      exclude: /node_modules/
-    }];
-    config.plugins = [
-      new webpack.HotModuleReplacementPlugin(),
-      new webpack.NoErrorsPlugin()
-    ];
+gulp.task('webpack-dev-server', function () {
+  var config = objectAssign({}, webpackConfig);
+  config.devtool = 'source-map';
+  config.debug = true;
+  config.entry = [
+    'webpack-dev-server/client?http://localhost:8090',
+    'webpack/hot/only-dev-server',
+    path.join(__dirname, '/app/index.js')
+  ];
+  config.module.loaders = [{
+    test: /\.js/,
+    loaders: ['react-hot', 'babel-loader'],
+    exclude: /node_modules/
+  }];
+  config.plugins = [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ];
 
-    new WebpackDevServer(webpack(config), {
-      publicPath: config.output.publicPath,
-      hot: true,
-      color: true,
-      stats: {
-        assets: true,
-        colors: true,
-        version: false,
-        hash: false,
-        timings: true,
-        chunks: false,
-        chunkModules: false
-      }
-    }).listen(8090, 'localhost', function (err) {
-      if (err) {
-        throw new $.util.PluginError('webpack-dev-server', err);
-      }
-      $.util.log('[webpack-dev-server]', 'started on port 8090');
-    });
-  }
-);
+  new WebpackDevServer(webpack(config), {
+    publicPath: config.output.publicPath,
+    hot: true,
+    color: true,
+    stats: {
+      assets: true,
+      colors: true,
+      version: false,
+      hash: false,
+      timings: true,
+      chunks: false,
+      chunkModules: false
+    }
+  }).listen(8090, 'localhost', function (err) {
+    if (err) {
+      throw new $.util.PluginError('webpack-dev-server', err);
+    }
+    $.util.log('[webpack-dev-server]', 'started on port 8090');
+  });
+});
 
 gulp.task('server', function (done) {
   // thank's https://github.com/kriasoft/react-starter-kit/blob/master/gulpfile.js#L127
@@ -169,7 +156,7 @@ gulp.task('server', function (done) {
   });
 });
 
-gulp.task('images', ['clean:img'], function () {
+gulp.task('images', function () {
   return gulp.src('app/images/**/*')
     .pipe($.imagemin({
       optimizationLevel: 3,
@@ -191,7 +178,7 @@ gulp.task('sass', function () {
     .pipe(gulp.dest('.tmp/css'));
 });
 
-gulp.task('styles', ['clean:css', 'sass'], function () {
+gulp.task('styles', ['sass'], function () {
   return gulp.src('.tmp/css/**/*.css')
     .pipe($.concatCss('styles.css'))
     .pipe(gulp.dest('dist/css'))
@@ -202,72 +189,66 @@ gulp.task('test:watch', function (done) {
   karma.start({configFile: path.join(__dirname, 'karma.conf.js')}, done);
 });
 
-gulp.task('dev', [
-  'images',
-  'styles',
-  'server',
-  'webpack-dev-server',
-  'connect',
-  'test:watch'
-], function () {
-  gulp.watch(CODE_FILES, ['lint:code']);
-  gulp.watch('app/images/**/*', ['images']);
-  gulp.watch('app/styles/**/*', ['styles']);
+gulp.task('dev', ['clean'], function () {
+  runSequence(
+    ['images', 'styles'],
+    ['server', 'webpack-dev-server'],
+    'connect',
+    function () {
+      gulp.watch(CODE_FILES, ['lint:code']);
+      gulp.watch('app/images/**/*', ['images']);
+      gulp.watch('app/styles/**/*', ['styles']);
+    }
+  );
 });
 
 // +-------------+
 // | BUILD TASKS |
 // +-------------+
 
-gulp.task(
-  'webpack:build',
-  ['lint:code', 'test:build', 'clean:js'],
-  function (callback) {
-    var config = objectAssign({}, webpackConfig);
-    config.plugins = [
-      new webpack.DefinePlugin({
-        'process.env': {
-          NODE_ENV: JSON.stringify('production')
-        }
-      }),
-      new webpack.optimize.DedupePlugin(),
-      new webpack.optimize.UglifyJsPlugin({
-        compress: {
-          warnings: false,
-          screw_ie8: true,
-          sequences: true,
-          dead_code: true,
-          drop_debugger: true,
-          comparisons: true,
-          conditionals: true,
-          evaluate: true,
-          booleans: true,
-          loops: true,
-          unused: true,
-          hoist_funs: true,
-          if_return: true,
-          join_vars: true,
-          cascade: true,
-          drop_console: true
-        },
-        output: {
-          comments: false
-        },
-        sourceMap: false
-      })
-    ];
-
-    webpack(config, function (err, stats) {
-      if (err) {
-        throw new $.util.PluginError('webpack:build', err);
+gulp.task('webpack:build', function (done) {
+  var config = objectAssign({}, webpackConfig);
+  config.plugins = [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
       }
-      $.util.log('[webpack:build]', stats.toString({
-        colors: true
-      }));
-      return callback();
-    });
-  }
-);
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        screw_ie8: true,
+        sequences: true,
+        dead_code: true,
+        drop_debugger: true,
+        comparisons: true,
+        conditionals: true,
+        evaluate: true,
+        booleans: true,
+        loops: true,
+        unused: true,
+        hoist_funs: true,
+        if_return: true,
+        join_vars: true,
+        cascade: true,
+        drop_console: true
+      },
+      output: {
+        comments: false
+      },
+      sourceMap: false
+    })
+  ];
+
+  webpack(config, function (err, stats) {
+    if (err) {
+      throw new $.util.PluginError('webpack:build', err);
+    }
+    $.util.log('[webpack:build]', stats.toString({colors: true}));
+    return done();
+  });
+});
 
 gulp.task('build:styles', ['styles'], function () {
   return gulp.src('dist/css/styles.css')
@@ -282,8 +263,11 @@ gulp.task('test:build', function (done) {
   }, done);
 });
 
-gulp.task('build', [
-  'build:styles',
-  'images',
-  'webpack:build'
-]);
+gulp.task('build', ['clean'], function (done) {
+  runSequence([
+    'build:styles',
+    'images',
+    'lint:code',
+    'test:build'
+  ], 'webpack:build', done);
+});
