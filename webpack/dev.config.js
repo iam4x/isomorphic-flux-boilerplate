740197a941ca0cadf7ee998fcbba9862b74d2b66
iam4x <iam4x@me.com>
2015-04-09 17:34:56 +0200

    fix: `scss/css` sourceMaps

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -39,7 +39,7 @@ export default {
       },
       {
         test: /\.scss$/,
-        loader: 'style!css!autoprefixer?browsers=last 2 version!sass?outputStyle=expanded&sourceMap'
+        loader: 'style!css?sourceMap!autoprefixer?browsers=last 2 version!sass?outputStyle=expanded&sourceMap'
       }
     ]
   },
