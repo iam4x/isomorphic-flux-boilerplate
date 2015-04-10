e9d54d8df0c6acaa4b92aa432f269a8d20fadfdd
iam4x <iam4x@me.com>
2015-04-10 18:29:31 +0200

    feat: add image example

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -7,6 +7,9 @@ export default React.createClass({
   render() {
     return (
       <header>
+        <h1 className='logo'>
+          React Isomorphic
+        </h1>
         <ul>
           <li><Link to='app'>Users</Link></li>
           <li><Link to='guides'>Guides</Link></li>
