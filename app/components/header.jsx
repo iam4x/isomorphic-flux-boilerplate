a6acbabfb87c833839534bb61f3c8de29f524370
iam4x <iam4x@me.com>
2015-04-10 23:45:54 +0200

    style: add header styles

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -3,14 +3,18 @@
 import React from 'react';
 import {Link} from 'react-router';
 
+if (process.env.BROWSER) {
+  require('styles/header.scss');
+}
+
 export default React.createClass({
   render() {
     return (
-      <header>
-        <h1 className='logo'>
+      <header className='app--header'>
+        <h1 className='app--logo'>
           React Isomorphic
         </h1>
-        <ul>
+        <ul className='app--navbar'>
           <li><Link to='app'>Users</Link></li>
           <li><Link to='guides'>Guides</Link></li>
         </ul>
