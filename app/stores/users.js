d33a3eaf44e8a3dd5ab6783a79560a2c38b0d7f4
iam4x <iam4x@me.com>
2015-04-11 14:31:50 +0200

    fix: don't remove added users, update the old ones

--- app/stores/users.js
+++ app/stores/users.js
@@ -1,5 +1,7 @@
 'use strict';
 
+import {isEmpty} from 'lodash';
+
 import alt from 'utils/alt';
 import UsersActions from 'actions/users';
 
@@ -24,7 +26,26 @@ class UsersStore {
   }
 
   onFetchSuccess(users) {
-    this.users = users;
+    if (isEmpty(this.users)) {
+      // just apply the new users
+      // this is called on every server rendering
+      this.users = users;
+    }
+    else {
+      const merged = this.users.slice();
+      users.forEach((user) => {
+        // update the most recent data into store
+        let match = merged.find((u) => u.seed === user.seed);
+        if (match) {
+          match = user;
+        }
+        // push the new user
+        else {
+          merged.push(user);
+        }
+      });
+      this.users = merged;
+    }
   }
 
   onFetchBySeedSuccess(user) {
