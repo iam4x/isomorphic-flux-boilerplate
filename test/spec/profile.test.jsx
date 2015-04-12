7beba7fed78f72e6ac584b68bb4b934affaabb39
iam4x <iam4x@me.com>
2015-04-12 21:01:16 +0200

    test: add profile component tests

---
+++ test/spec/profile.test.jsx
@@ -0,0 +1,65 @@
+'use strict';
+
+import chai from 'chai';
+import React from 'react/addons';
+import reactRouterStub from '../utils/stub-router-context';
+import {capitalize} from 'lodash';
+
+import alt from 'utils/alt';
+import Profile from 'components/profile';
+import UsersStore from 'stores/users';
+
+import {users} from 'data/users.json';
+
+const should = chai.should();
+const seed = '7729a1ef4ba6ef68';
+
+describe('Profile', () => {
+
+  let instance;
+  const TestUtils = React.addons.TestUtils;
+
+  beforeEach(() => {
+    // clean stores
+    alt.flush();
+    const Stubbed = reactRouterStub(Profile, {}, {
+      getCurrentParams() {
+        return {seed};
+      }
+    });
+    instance = TestUtils.renderIntoDocument(React.createElement(Stubbed));
+  });
+
+  afterEach(() => {
+    if (instance && instance.isMounted()) {
+      React.unmountComponentAtNode(instance.getDOMNode());
+    }
+  });
+
+  it('should render user name after request', (done) => {
+    const handleChange = () => {
+      const fullName = TestUtils.findRenderedDOMComponentWithTag(instance, 'h2');
+      should.exist(fullName);
+      const user = users.find((user) => user.seed === seed);
+      should.exist(user.user);
+      fullName.getDOMNode().textContent.should.eql(`${capitalize(user.user.name.first)} ${capitalize(user.user.name.last)}`);
+      UsersStore.unlisten(handleChange);
+      return done();
+    };
+    UsersStore.listen(handleChange);
+  });
+
+  it('should render user picture after request', (done) => {
+    const handleChange = () => {
+      const picture = TestUtils.findRenderedDOMComponentWithTag(instance, 'img');
+      should.exist(picture);
+      const user = users.find((user) => user.seed === seed);
+      should.exist(user.user);
+      picture.getDOMNode().src.should.eql(user.user.picture.medium);
+      UsersStore.unlisten(handleChange);
+      return done();
+    };
+    UsersStore.listen(handleChange);
+  });
+
+});
