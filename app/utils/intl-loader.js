a946d693b874977da759ed53dc8af7c65ac931a8
iam4x <iam4x@me.com>
2015-04-15 13:25:18 +0200

    feat: add spinner on changing lang

--- app/utils/intl-loader.js
+++ app/utils/intl-loader.js
@@ -2,14 +2,18 @@
 
 import debug from 'debug';
 
+import RequestsActions from 'actions/requests';
+
 const loaders = {
   en(callback) {
+    RequestsActions.start();
     if (!window.Intl) {
       require.ensure([
         'intl/Intl',
         'intl/locale-data/jsonp/en.js',
         'data/en'
       ], (require) => {
+        RequestsActions.success();
         require('intl/Intl');
         require('intl/locale-data/jsonp/en.js');
         const lang = require('data/en');
@@ -18,18 +22,21 @@ const loaders = {
     }
     else {
       require.ensure(['data/en'], (require) => {
+        RequestsActions.success();
         const lang = require('data/en');
         return callback(lang);
       });
     }
   },
   fr(callback) {
+    RequestsActions.start();
     if (!window.Intl) {
       require.ensure([
         'intl/Intl',
         'intl/locale-data/jsonp/fr.js',
         'data/fr'
       ], (require) => {
+        RequestsActions.success();
         require('intl/Intl');
         require('intl/locale-data/jsonp/fr.js');
         const lang = require('data/fr');
@@ -38,6 +45,7 @@ const loaders = {
     }
     else {
       require.ensure(['data/fr'], (require) => {
+        RequestsActions.success();
         const lang = require('data/fr');
         return callback(lang);
       });
