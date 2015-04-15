5e3dd6a5b71981eabb85025d19c428cf007f16df
iam4x <iam4x@me.com>
2015-04-15 11:59:42 +0200

    style: add `active` class on active locale

--- app/components/shared/lang-picker.jsx
+++ app/components/shared/lang-picker.jsx
@@ -1,8 +1,10 @@
 'use strict';
 
 import React from 'react';
+import classNames from 'classnames';
 
 import LocaleActions from 'actions/locale';
+import LocaleStore from 'stores/locale';
 
 if (process.env.BROWSER) {
   require('styles/lang-picker.scss');
@@ -13,19 +15,25 @@ export default React.createClass({
   handleClick(locale) {
     LocaleActions.switchLocale(locale);
   },
+  renderLocales(locales) {
+    const activeLocale = LocaleStore.getLocale();
+    return locales.map((locale, index) => {
+      const klass = classNames({active: locale === activeLocale});
+      return (
+        <li key={index}>
+          <a
+            className={klass}
+            onClick={this.handleClick.bind(this, locale)}>
+            {locale}
+          </a>
+        </li>
+      );
+    });
+  },
   render() {
     return (
-      <ul className='lang--picker'>
-        <li>
-          <a onClick={this.handleClick.bind(this, 'en')}>
-            EN
-          </a>
-        </li>
-        <li>
-          <a onClick={this.handleClick.bind(this, 'fr')}>
-            FR
-          </a>
-        </li>
+      <ul className='lang--picker un-select'>
+        {this.renderLocales(['en', 'fr'])}
       </ul>
     );
   }
