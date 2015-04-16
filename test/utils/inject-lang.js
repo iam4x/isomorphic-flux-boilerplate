f94da1638b75f34f31f9d83461b5949fb582b4b7
iam4x <iam4x@me.com>
2015-04-16 20:27:52 +0200

    test: fix `injectLang` test utils

--- test/utils/inject-lang.js
+++ test/utils/inject-lang.js
@@ -1,23 +1,11 @@
 'use strict';
 
-import alt from 'utils/alt';
-import LocaleActions from 'actions/locale';
-import LocaleStore from 'stores/locale';
+export default (flux, locale='en') => {
+  const {messages} = require(`data/${locale}`);
 
-export default {
-  initialize() {
-    // initialize LocaleStore with lang=en
-    return this.loadLang('en');
-  },
-  getProps() {
-    return LocaleStore.getState();
-  },
-  loadLang(locale) {
-    const {messages} = require(`data/${locale}`);
-    LocaleActions.switchLocaleSuccess({locale, messages});
-  },
-  clean() {
-    // reset LocaleStore
-    alt.recycle('LocaleStore');
-  }
+  flux
+    .getActions('locale')
+    .switchLocaleSuccess({locale, messages});
+
+  return flux.getStore('locale').getState();
 };
