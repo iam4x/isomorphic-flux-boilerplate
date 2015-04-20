27873a5826e782f9096e213503747989a1767a2f
iam4x <iam4x@me.com>
2015-04-20 19:19:57 +0200

    test: add `image-resolver` tests

--- app/utils/image-resolver.js
+++ app/utils/image-resolver.js
@@ -2,14 +2,14 @@
 
 import debug from 'debug';
 
-export default (imagePath) => {
-  if (process.env.BROWSER) {
+export default (imagePath, webpackStats, force) => {
+  if (process.env.BROWSER && !force) {
     debug('dev')('`image-resolver` should not be used in browser, something went wrong');
     throw new Error('image-resolver called on browser');
   }
   else {
     // Load images compiled from `webpack-stats`
-    const images = require('../../server/webpack-stats.json').images;
+    const images = webpackStats.images || require('../../server/webpack-stats.json').images;
 
     // Find the correct image
     const regex = new RegExp(`${imagePath}$`);
---
+++ test/spec/utils/image-resolver.test.js
@@ -0,0 +1,32 @@
+'use strict';
+
+import chai from 'chai';
+import imageResolver from 'utils/image-resolver';
+
+const should = chai.should();
+
+const webpackStats = {
+  images: [
+    {
+      original: 'logo.png',
+      compiled: '2uierwi2.png'
+    }
+  ]
+};
+
+describe('ImageResolver', () => {
+
+  it('should resolve for the correct image', () => {
+    const image = webpackStats.images[0];
+    const imagePath = imageResolver(image.original, webpackStats, true);
+    should.exist(imagePath);
+    imagePath.should.be.eql(image.compiled);
+  });
+
+  it('should resolve to empty string when no image is matched', () => {
+    const imagePath = imageResolver('foo.png', webpackStats, true);
+    should.exist(imagePath);
+    imagePath.should.be.eql('');
+  });
+
+});
