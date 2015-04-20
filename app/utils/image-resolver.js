7d0e14853a24c64a25fb7421d46bab140b7028d4
iam4x <iam4x@me.com>
2015-04-20 23:21:33 +0200

    fix: circular dep which make webpack to re-compile loop

--- app/utils/image-resolver.js
+++ app/utils/image-resolver.js
@@ -2,14 +2,14 @@
 
 import debug from 'debug';
 
-export default (imagePath, webpackStats, force) => {
-  if (process.env.BROWSER && !force) {
+export default (imagePath) => {
+  if (process.env.BROWSER) {
     debug('dev')('`image-resolver` should not be used in browser, something went wrong');
     throw new Error('image-resolver called on browser');
   }
   else {
     // Load images compiled from `webpack-stats`
-    const images = webpackStats || require('../../server/webpack-stats.json').images;
+    const images = require('../../server/webpack-stats.json').images;
 
     // Find the correct image
     const regex = new RegExp(`${imagePath}$`);
