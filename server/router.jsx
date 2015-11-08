5f8f4a3f938e17e8a63052a7cb4e161bafabf04a
MaxSvargal <maxsvargal@gmail.com>
2015-11-08 18:25:21 +0700

    Move media query front script hoock to separated file

--- server/router.jsx
+++ server/router.jsx
@@ -1,4 +1,5 @@
 import debug from 'debug';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 import createFlux from 'flux/createFlux';
 
 import ApiClient from '../shared/api-client';
@@ -26,13 +27,17 @@ export default function *() {
     // Assets name are found into `webpack-stats`
     const assets = require('./webpack-stats.json');
 
+    // Refresh page at a first time after set screen resolution for server
+    const extractor = new ExtractTextPlugin('./views/media-query-refresh.js');
+    const mediaQueryRefreshScript = extractor.extract();
+
     // Don't cache assets name on dev
     if (process.env.NODE_ENV === 'development') {
       delete require.cache[require.resolve('./webpack-stats.json')];
     }
 
     debug('dev')('return html content');
-    yield this.render('main', { body, assets, locale, title });
+    yield this.render('main', { body, assets, locale, title, mediaQueryRefreshScript });
   } catch (err) {
     // Render 500 error page from server
     const { error, redirect } = err;
