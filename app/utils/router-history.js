2b49ee65f4339925389a28397664651df3d9941d
iam4x <iam4x@me.com>
2015-12-08 00:36:35 +0100

    fix(scroll): scroll back to top on route change

--- app/utils/router-history.js
+++ app/utils/router-history.js
@@ -1,5 +1,6 @@
 import createBrowserHistory from 'history/lib/createBrowserHistory';
+import useStandardScroll from 'scroll-behavior/lib/useStandardScroll';
 
 // Export history module as singleton to be used outside of components
 // for instance after a login into flux stores
-export default createBrowserHistory();
+export default useStandardScroll(createBrowserHistory)();
