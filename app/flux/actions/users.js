0861f82969043399dc82dbece9f707d710669aa7
iam4x <iam4x@me.com>
2015-12-15 11:57:20 +0100

    feat(alt-0.18): call `alt-resolver` with new alt fn return

--- app/flux/actions/users.js
+++ app/flux/actions/users.js
@@ -9,31 +9,37 @@ class UsersActions {
   }
 
   index() {
-    this._flux.resolve(async (done) => {
-      try {
-        this._flux.getActions('requests').start();
-        const response = await this._flux.request({ url: '/users' });
-        this.indexSuccess(response);
-      } catch (error) {
-        this.indexFail({ error });
-      }
-      this._flux.getActions('requests').stop();
-      return done();
-    });
+    // You need to return a fn in actions
+    // to get alt instance as second parameter to access
+    // `alt-resolver` and the ApiClient
+    return (dispatch, alt) =>
+      // We use `alt-resolver` from the boilerplate
+      // to indicate the server we need to resolve
+      // this data before server side rendering
+      alt.resolve(async () => {
+        try {
+          alt.getActions('requests').start();
+          const response = await alt.request({ url: '/users' });
+          this.indexSuccess(response);
+        } catch (error) {
+          this.indexFail({ error });
+        }
+        alt.getActions('requests').stop();
+      });
   }
 
   show(seed) {
-    this._flux.resolve(async (done) => {
-      try {
-        this._flux.getActions('requests').start();
-        const response = await this._flux.request({ url: '/users/' + seed });
-        this.showSuccess(response);
-      } catch (error) {
-        this.showFail({ error });
-      }
-      this._flux.getActions('requests').stop();
-      return done();
-    });
+    return (dispatch, alt) =>
+      alt.resolve(async () => {
+        try {
+          alt.getActions('requests').start();
+          const response = await alt.request({ url: `/users/${seed}` });
+          this.showSuccess(response);
+        } catch (error) {
+          this.showFail({ error });
+        }
+        alt.getActions('requests').stop();
+      });
   }
 
 }
