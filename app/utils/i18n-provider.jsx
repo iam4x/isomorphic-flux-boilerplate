6f2767b77243814478e70664ed81c5f0dea6c3b7
mayas <mayas.haddad@save.co>
2016-05-17 14:23:56 +0200

    upgrade react-intl

---
+++ app/utils/i18n-provider.jsx
@@ -0,0 +1,37 @@
+import debug from 'debug'
+
+import React, { Component, PropTypes } from 'react'
+import { toPath } from 'lodash'
+
+class I18nProvider extends Component {
+
+  static propTypes = { children: PropTypes.node.isRequired }
+
+  static childContextTypes = {
+    i18n: PropTypes.func.isRequired
+  }
+
+  static contextTypes = {
+    intl: PropTypes.object.isRequired
+  }
+
+  getChildContext() {
+    return { i18n: this.i18n }
+  }
+
+  i18n = (key, values) => {
+    try {
+      return toPath(key).reduce((prec, x) => prec[x], this.context.intl.messages)
+    } catch (error) {
+      debug('dev')(error)
+      return `translation missing (${this.state.locales[0]}): ${key}`
+    }
+  }
+
+  render() {
+    const { children } = this.props
+    return <span>{ children }</span>
+  }
+}
+
+export default I18nProvider
