b503e0428efc9f5059264edb80bb9282cae721df
mayas <mayas.haddad@save.co>
2016-05-18 17:51:43 +0200

    chore: update locales messages

--- app/data/en.js
+++ app/data/en.js
@@ -1,47 +1,29 @@
 export default {
   messages: {
-    header: {
-      users: 'Users List',
-      guides: 'Guides',
-      account: 'Account',
-      logout: 'Logout',
-      login: 'Login'
-    },
-    guides: {
-      'page-title': 'Guides'
-    },
-    protected: {
-      'page-title': 'Protected Page'
-    },
-    profile: {
-      'page-title': 'Profile - {fullName}',
-      'not-found-page-title': 'User profile not found'
-    },
-    users: {
-      'page-title': 'Users',
-      title: 'Some random users',
-      email: 'Email address',
-      actions: 'Actions',
-      add: 'Add random user',
-      profile: 'Profile'
-    },
-    login: {
-      help: 'Any credentials will work, it creates a fake session for example.',
-      username: {
-        label: 'Login',
-        placeholder: 'example@app.com'
-      },
-      password: {
-        label: 'Password'
-      },
-      submit: 'Submit'
-    },
-    routes: {
-      users: '/users',
-      guides: '/guides',
-      login: '/login',
-      profile: '/profile/:seed',
-      account: '/account'
-    }
+    'header.users': 'Users List',
+    'header.guides': 'Guides',
+    'header.account': 'Account',
+    'header.logout': 'Logout',
+    'header.login': 'Login',
+    'guides.page-title': 'Guides',
+    'protected.page-title': 'Protected Page',
+    'profile.page-title': 'Profile - {fullName}',
+    'profile.not-found-page-title': 'User profile not found',
+    'users.page-title': 'Users',
+    'users.title': 'Some random users',
+    'users.email': 'Email address',
+    'users.actions': 'Actions',
+    'users.add': 'Add random user',
+    'users.profile': 'Profile',
+    'login.help': 'Any credentials will work, it creates a fake session for example.',
+    'login.username.label': 'Login',
+    'login.username.placeholder': 'example@app.com',
+    'login.password.label': 'Password',
+    'login.submit': 'Submit',
+    'routes.users': '/users',
+    'routes.guides': '/guides',
+    'routes.login': '/login',
+    'routes.profile': '/profile/:seed',
+    'routes.account': '/account'
   }
 };
--- app/data/fr.js
+++ app/data/fr.js
@@ -1,47 +1,29 @@
 export default {
   messages: {
-    header: {
-      users: 'Utilisateurs',
-      guides: 'Guides',
-      account: 'Mon compte',
-      logout: 'Se déconnecter',
-      login: 'Se connecter'
-    },
-    guides: {
-      'page-title': 'Guides'
-    },
-    protected: {
-      'page-title': 'Page protégée'
-    },
-    profile: {
-      'page-title': 'Profil - {fullName}',
-      'not-found-page-title': 'User profile not found'
-    },
-    users: {
-      'page-title': 'Utilisateurs',
-      title: 'Des utilisateurs au hasard',
-      email: 'Adresse email',
-      actions: 'Opérations',
-      add: 'Ajouter un utilisateur',
-      profile: 'Profil'
-    },
-    login: {
-      help: 'N\'importe quels informations fonctionneront. C\'est un exemple qui créér une fausse session.',
-      username: {
-        label: 'Utilisateur',
-        placeholder: 'exemple@app.fr'
-      },
-      password: {
-        label: 'Mot de passe'
-      },
-      submit: 'Se connecter'
-    },
-    routes: {
-      users: '/utilisateurs',
-      guides: '/guides',
-      login: '/connexion',
-      profile: '/profil/:seed',
-      account: '/mon-compte'
-    }
+    'header.users': 'Utilisateurs',
+    'header.guides': 'Guides',
+    'header.account': 'Mon compte',
+    'header.logout': 'Se déconnecter',
+    'header.login': 'Se connecter',
+    'guides.page-title': 'Guides',
+    'protected.page-title': 'Page protégée',
+    'profile.page-title': 'Profil - {fullName}',
+    'profile.not-found-page-title': 'User profile not found',
+    'users.page-title': 'Utilisateurs',
+    'users.title': 'Des utilisateurs au hasard',
+    'users.email': 'Adresse email',
+    'users.actions': 'Opérations',
+    'users.add': 'Ajouter un utilisateur',
+    'users.profile': 'Profil',
+    'login.help': 'N\'importe quels informations fonctionneront. C\'est un exemple qui créér une fausse session.',
+    'login.username.label': 'Utilisateur',
+    'login.username.placeholder': 'exemple@app.fr',
+    'login.password.label': 'Mot de passe',
+    'login.submit': 'Se connecter',
+    'routes.users': '/utilisateurs',
+    'routes.guides': '/guides',
+    'routes.login': '/connexion',
+    'routes.profile': '/profil/:seed',
+    'routes.account': '/mon-compte'
   }
 };
